86be321a2db7fd9afa6ff95f173ce6908e379c35
ddarahakit <ddarahakit@gmail.com>
2022-05-19 13:43:26 +0900

    test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.9
 
-WORKDIR /code
-COPY requirements.txt /code/
+WORKDIR /apps
+COPY requirements.txt /apps/
 RUN pip install -r requirements.txt
-COPY . /code/
+COPY . /apps/
+EXPOSE 8000
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "--workers", "3", "config.wsgi.application"]
--- config/settings.py
+++ config/settings.py
@@ -15,14 +15,11 @@ import os
 import environ
 
 env = environ.Env(
-    # set casting, default value
     DEBUG=(bool, False)
 )
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# reading .env file
 environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
 )
